f068b5ba70b8791c763cfdee569252eb99c99fc5
kravetst <taraskravetc28@gmail.com>
2025-05-18 11:59:00 +0300

    Повідомлеpolished the code to perfection

--- app/main.py
+++ app/main.py
@@ -1,6 +1,37 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_storage = {}
+
+    def wrapper(*args, **kwargs) -> Any:
+        key = (args, tuple(sorted(kwargs.items())))
+        if key in cache_storage:
+            print("Getting from cache")
+            return cache_storage[key]
+
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            cache_storage[key] = result
+            return result
+
+    return wrapper
+
+
+@cache
+def long_time_func(base: int, exponet1: int, exponet2: int) -> int:
+    return (base ** exponet1 ** exponet2) % (base * exponet2)
+
+
+@cache
+def long_time_func_2(numbers: tuple, power: int) -> list:
+    return [number ** power for number in numbers]
+
+
+long_time_func(1, 2, 3)
+long_time_func(2, 2, 3)
+long_time_func_2((5, 6, 7), 5)
+long_time_func(1, 2, 3)
+long_time_func_2((5, 6, 7), 10)
+long_time_func_2((5, 6, 7), 10)
